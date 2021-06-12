4a77319cefbcf0e8eeff9a326eb9a4e7be7bf0c8
Claksoo <37747029+Claksoo@users.noreply.github.com>
2021-06-12 16:41:35 +0200

    Update mission_params.hpp
    Zeus für steckegeblieben AI

--- lib_LennLite.Chernarus/ui/mission_params.hpp
+++ lib_LennLite.Chernarus/ui/mission_params.hpp
@@ -297,13 +297,13 @@ class Params {
         title = $STR_PARAM_COMMANDERZEUS;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 0;
+        default = 1;
     };
     class LimitedZeus {
         title = $STR_PARAM_LIMITEDZEUS;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 1;
+        default = 0;
     };
     class ZeusAddEnemies {
         title = $STR_PARAM_ZEUSADDENEMIES;
